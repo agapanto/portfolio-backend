6585f8b9eee8f30073519b6e1f82584d720556e4
mcueto <cueto@live.cl>
2019-04-30 02:07:48 -0400

    Create Portfolio model on agapanto_portfolios app

---
+++ agapanto_portfolios/models.py
@@ -0,0 +1,65 @@
+"""agapanto_portfolios models."""
+# import uuid
+from django.conf import settings
+from django.db import models
+# from django.contrib.postgres.fields import JSONField
+from rest_framework_apicontrol.mixins import (
+    STATUS_FIELD_MAX_LENGTH,
+    # ActiveModelMixin,
+    # EnabledModelMixin,
+    PerAppModelMixin,
+    InstanceStatusModelMixin,
+    TrackableModelMixin,
+    UniqueIDModelMixin,
+)
+
+PORTFOLIO_STATUS_CHOICES = (
+    ('hidden', 'hidden'),
+    ('published', 'published'),
+)
+
+PORTFOLIO_READ_ACCESS_CHOICES = (
+    ('owner', 'owner'),
+    ('group', 'group'),
+    ('public', 'public'),
+)
+
+PORTFOLIO_WRITE_ACCESS_CHOICES = (
+    ('owner', 'owner'),
+    ('group', 'group'),
+    ('public', 'public'),
+)
+
+
+class Portfolio(InstanceStatusModelMixin,
+                TrackableModelMixin,
+                UniqueIDModelMixin):
+    """Portfolio: group of items to show to others."""
+
+    name = models.CharField(
+        max_length=64
+    )
+    description = models.TextField()
+    image = models.ImageField()
+    current_status = models.CharField(
+        max_length=STATUS_FIELD_MAX_LENGTH,
+        choices=PORTFOLIO_STATUS_CHOICES,
+    )
+    read_access = models.CharField(
+        max_length=STATUS_FIELD_MAX_LENGTH,
+        choices=PORTFOLIO_READ_ACCESS_CHOICES
+    )
+    write_access = models.CharField(
+        max_length=STATUS_FIELD_MAX_LENGTH,
+        choices=PORTFOLIO_WRITE_ACCESS_CHOICES
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        pass
+
+    def __str__(self):
+        return self.unique_id
