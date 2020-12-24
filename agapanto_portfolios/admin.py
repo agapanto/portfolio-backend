dba5bd9dce908d106bce713a7357b967d3b568a6
Marcelo Cueto <cueto@live.cl>
2020-12-24 19:53:46 -0300

    Set TrackableModelAdmin to models registered on admin.py

--- agapanto_portfolios/admin.py
+++ agapanto_portfolios/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
+from rest_framework_apicontrol.admin import (
+    TrackableModelAdmin,
+)
 from .models import (
     Portfolio,
     PortfolioItem,
 )
 
 
-admin.site.register(Portfolio)
-admin.site.register(PortfolioItem)
+admin.site.register(Portfolio, TrackableModelAdmin)
+admin.site.register(PortfolioItem, TrackableModelAdmin)
